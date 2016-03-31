de2ca3ddf71a1e68e462a9995eda04375e04f393
r-leitao <raphael.leitao@hotmail.fr>
2016-03-31 23:38:31 +0200

    menu : ajout de la première partie du menu

--- menuPompier.php
+++ menuPompier.php
@@ -1,85 +1,114 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Gentallela Alela! | </title>
-
-  <!-- Bootstrap core CSS -->
-
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-
-  <link href="fonts/css/font-awesome.min.css" rel="stylesheet">
-  <link href="css/animate.min.css" rel="stylesheet">
-
-  <!-- Custom styling plus plugins -->
-  <link href="css/custom.css" rel="stylesheet">
-  <link href="css/icheck/flat/green.css" rel="stylesheet">
-
-
-  <script src="js/jquery.min.js"></script>
-
-  <!--[if lt IE 9]>
-        <script src="../assets/js/ie8-responsive-file-warning.js"></script>
-        <![endif]-->
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
-</head>
-
-
-<body class="nav-md">
-
-  <div class="container body">
-
-
-    <div class="main_container">
-
-      <div class="col-md-3 left_col">
-        <div class="left_col scroll-view">
-
-          <div class="navbar nav_title" style="border: 0;">
-            <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
-          </div>
-          <div class="clearfix"></div>
-
-          <!-- menu prile quick info -->
-          <div class="profile">
-            <div class="profile_pic">
-              <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-            </div>
-            <div class="profile_info">
-              <span>Welcome,</span>
-              <h2>John Doe</h2>
-            </div>
-          </div>
-          <!-- /menu prile quick info -->
-
-          <br />
-
-          <!-- sidebar menu -->
+<!-- sidebar menu -->
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
             <div class="menu_section">
               <h3>General</h3>
               <ul class="nav side-menu">
-                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-home"></i> Bilans <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu" style="display: none">
-                    <li><a href="index.html">Dashboard</a>
-                    </li>
-                    <li><a href="index2.html">Dashboard2</a>
-                    </li>
-                    <li><a href="index3.html">Dashboard3</a>
-                    </li>
+                    <li><a href="index.html">Bilan circonstanciel</a></li>
+                    <li><a href="index2.html">Bilan d'urgence vitale</a></li>
+                    <li><a href="index3.html">Bilan complémentaire suite à un malaise ou l’aggravation d’une maladie</a></li>
+                    <li><a href="index3.html">Bilan complémentaire suite à un traumatisme</a></li>
+                    <li><a href="index3.html">Surveillance de la victime</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Protection et sécurité <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Sécurité sur intervention</a></li>
+                    <li><a href="index2.html">Protection d'un accident électrique</a></li>
+                    <li><a href="index3.html">Protection d'un accident de la route</a></li>
+                    <li><a href="index3.html">Protection contre le monoxyde de carbone</a></li>
+                    <li><a href="index3.html">Protection contre l’incendie</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Hygiène et asepsie <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Accident d’exposition à un risque viral</a></li>
+                    <li><a href="index2.html">Précautions standards contre le risque infectieux</a></li>
+                    <li><a href="index3.html">Précautions particulières contre le risque infectieux</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Urgences vitales <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Arrêt cardiaque chez l'adulte</a></li>
+                    <li><a href="index2.html">Arrêt cardiaque chez l'adulte en sauveteur isolé</a></li>
+                    <li><a href="index3.html">Arrêt cardiaque chez l'enfant ou le nourisson</a></li>
+                    <li><a href="index3.html">Arrêt cardiaque chez l'enfant ou le nourisson en sauveteur isolé</a></li>
+                    <li><a href="index3.html">Détresse circulatoire</a></li>
+                    <li><a href="index3.html">Détresse neurologique</a></li>
+                    <li><a href="index3.html">Détresse respiratoire</a></li>
+                    <li><a href="index3.html">Hémorragie externe</a></li>
+                    <li><a href="index3.html">Hémorragie extériorisée par la bouche</a></li>
+                    <li><a href="index3.html">Hémorragie extériorisée par le nez</a></li>
+                    <li><a href="index3.html">Hémorragie vaginale</a></li>
+                    <li><a href="index3.html">Hémorragie extériorisées - Autres types</a></li>
+                    <li><a href="index3.html">Obstruction partielle des voies aériennes</a></li>
+                    <li><a href="index3.html">Obstruction totale des voies aériennes</a></li>
+                    <li><a href="index3.html">Perte de connaissance</a></li>
+                    <li><a href="index3.html">Perte de connaissance en sauveteur isolé</a></li>
+                    <li><a href="index3.html">Section de membre</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Malaises et affections spécifiques <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Crise convulsive généralisée</a></li>
+                    <li><a href="index2.html">Crise d’asthme</a></li>
+                    <li><a href="index3.html">Malaise diabétique</a></li>
+                    <li><a href="index3.html">Malaise et aggravation de maladie</a></li>
+                    <li><a href="index3.html">Réaction allergique</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Atteintes circonstancielles <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Accident électrique</a></li>
+                    <li><a href="index2.html">Accidents liés à la plongée</a></li>
+                    <li><a href="index3.html">Accouchement inopiné</a></li>
+                    <li><a href="index3.html">Prise en charge du nouveau né à la naissance</a></li>
+                    <li><a href="index3.html">Crampe</a></li>
+                    <li><a href="index3.html">Insolation</a></li>
+                    <li><a href="index3.html">Hyperthermie maligne d’effort ou épuisement à la chaleur</a></li>
+                    <li><a href="index3.html">Compression de membre</a></li>
+                    <li><a href="index3.html">Effet de souffle</a></li>
+                    <li><a href="index3.html">Gelures</a></li>
+                    <li><a href="index3.html">Hypothermie</a></li>
+                    <li><a href="index3.html">Intoxication</a></li>
+                    <li><a href="index3.html">Intoxication en environnement toxique</a></li>
+                    <li><a href="index3.html">Noyade</a></li>
+                    <li><a href="index3.html">Pendaison, strangulation</a></li>
+                    <li><a href="index3.html">Piqûres ou morsures</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Traumatismes <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Brûlure chimique</a></li>
+                    <li><a href="index.html">Brûlure électrique</a></li>
+                    <li><a href="index.html">Brûlure interne par ingestion</a></li>
+                    <li><a href="index.html">Brûlure interne par inhalation</a></li>
+                    <li><a href="index.html">Brûlure thermique</a></li>
+                    <li><a href="index.html">Plaie</a></li>
+                    <li><a href="index.html">Traumatisme de l’abdomen</a></li>
+                    <li><a href="index.html">Traumatisme du bassin</a></li>
+                    <li><a href="index.html">Traumatisme du crâne</a></li>
+                    <li><a href="index.html">Traumatisme du dos et du cou</a></li>
+                    <li><a href="index.html">Traumatisme du thorax</a></li>
+                    <li><a href="index.html">Traumatisme des membres</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-home"></i> Souffrance psychique et comportements inhabituels <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Agitation</a></li>
+                    <li><a href="index.html">Agressivité</a></li>
+                    <li><a href="index.html">Anxiété</a></li>
+                    <li><a href="index.html">Etat de stupeur</a></li>
+                    <li><a href="index.html">Agression sexuelle</a></li>
+                    <li><a href="index.html">Attroupement de personnes</a></li>
+                    <li><a href="index.html">Crise suicidaire</a></li>
+                    <li><a href="index.html">Décès</a></li>
+                    <li><a href="index.html">Délire</a></li>
+                    <li><a href="index.html">Evénement traumatisant</a></li>
+                    <li><a href="index.html">Mort inattendue et inexpliquée du nourrisson</a></li>
+                    <li><a href="index.html">Souffrance psychique</a></li>
                   </ul>
                 </li>
                 <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
@@ -313,93 +342,4 @@
         </div>
 
       </div>
-      <!-- /top navigation -->
 
-      <!-- page content -->
-      <div class="right_col" role="main">
-        <div class="">
-          <div class="page-title">
-            <div class="title_left">
-              <h3>Plain Page</h3>
-            </div>
-
-            <div class="title_right">
-              <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                <div class="input-group">
-                  <input type="text" class="form-control" placeholder="Search for...">
-                  <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">Go!</button>
-                        </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="clearfix"></div>
-
-          <div class="row">
-
-            <div class="col-md-12 col-sm-12 col-xs-12">
-              <div class="x_panel" style="height:600px;">
-                <div class="x_title">
-                  <h2>Plain Page</h2>
-                  <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                      <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Settings 1</a>
-                        </li>
-                        <li><a href="#">Settings 2</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                    </li>
-                  </ul>
-                  <div class="clearfix"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- footer content -->
-        <footer>
-          <div class="copyright-info">
-            <p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-            </p>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-
-      </div>
-      <!-- /page content -->
-    </div>
-
-  </div>
-
-  <div id="custom_notifications" class="custom-notifications dsp_none">
-    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
-    </ul>
-    <div class="clearfix"></div>
-    <div id="notif-group" class="tabbed_notifications"></div>
-  </div>
-
-  <script src="js/bootstrap.min.js"></script>
-
-  <!-- bootstrap progress js -->
-  <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
-  <script src="js/nicescroll/jquery.nicescroll.min.js"></script>
-  <!-- icheck -->
-  <script src="js/icheck/icheck.min.js"></script>
-
-  <script src="js/custom.js"></script>
-
-  <!-- pace -->
-  <script src="js/pace/pace.min.js"></script>
-
-</body>
-
-</html>
---
+++ plain_page.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Gentallela Alela! | </title>
+
+  <!-- Bootstrap core CSS -->
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="fonts/css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet">
+
+  <!-- Custom styling plus plugins -->
+  <link href="css/custom.css" rel="stylesheet">
+  <link href="css/icheck/flat/green.css" rel="stylesheet">
+
+
+  <script src="js/jquery.min.js"></script>
+
+  <!--[if lt IE 9]>
+        <script src="../assets/js/ie8-responsive-file-warning.js"></script>
+        <![endif]-->
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+
+<body class="nav-md">
+
+  <div class="container body">
+
+
+    <div class="main_container">
+
+      <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+
+          <div class="navbar nav_title" style="border: 0;">
+            <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
+          </div>
+          <div class="clearfix"></div>
+
+          <!-- menu prile quick info -->
+          <div class="profile">
+            <div class="profile_pic">
+              <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+            </div>
+            <div class="profile_info">
+              <span>Welcome,</span>
+              <h2>John Doe</h2>
+            </div>
+          </div>
+          <!-- /menu prile quick info -->
+
+          <br />
+
+          <!-- sidebar menu -->
+          <?php include './menuPompier.php'; ?>
+      <!-- /top navigation -->
+
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <div class="">
+          <div class="page-title">
+            <div class="title_left">
+              <h3>Plain Page</h3>
+            </div>
+
+            <div class="title_right">
+              <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search for...">
+                  <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="clearfix"></div>
+
+          <div class="row">
+
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="x_panel" style="height:600px;">
+                <div class="x_title">
+                  <h2>Plain Page</h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Settings 1</a>
+                        </li>
+                        <li><a href="#">Settings 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12 col-sm-12 col-xs-12">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- footer content -->
+        <footer>
+          <div class="copyright-info">
+            <p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+            </p>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+
+      </div>
+      <!-- /page content -->
+    </div>
+
+  </div>
+
+  <div id="custom_notifications" class="custom-notifications dsp_none">
+    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+    </ul>
+    <div class="clearfix"></div>
+    <div id="notif-group" class="tabbed_notifications"></div>
+  </div>
+
+  <script src="js/bootstrap.min.js"></script>
+
+  <!-- bootstrap progress js -->
+  <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
+  <script src="js/nicescroll/jquery.nicescroll.min.js"></script>
+  <!-- icheck -->
+  <script src="js/icheck/icheck.min.js"></script>
+
+  <script src="js/custom.js"></script>
+
+  <!-- pace -->
+  <script src="js/pace/pace.min.js"></script>
+
+</body>
+
+</html>
